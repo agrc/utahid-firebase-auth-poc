c4bef07099caf6decb394369741e1c0ef7b3fa35
stdavis <stdavis@utah.gov>
2022-05-25 08:52:48 -0600

    feat: secure arcgis server service

---
+++ functions/secrets.sample.js
@@ -0,0 +1,6 @@
+export default {
+  arcgisServer: {
+    username: '',
+    password: '',
+  }
+}
--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,53 @@ function App() {
   const authRef = useRef(null);
   const functionsRef = useRef(null);
   const [user, setUser] = useState(null);
+  const interceptor = useRef();
+
+  const initMap = () => {
+    require([
+      "esri/config",
+      "esri/Map",
+      "esri/views/MapView",
+
+      "esri/layers/FeatureLayer"
+
+    ], function(esriConfig,Map, MapView, FeatureLayer) {
+      if (interceptor.current) {
+        esriConfig.request.interceptors.push(interceptor.current);
+      }
+
+      const map = new Map({
+        basemap: "streets"
+      });
+
+      const view = new MapView({
+        container: "mapDiv",
+        map: map,
+        center: [-112.60543,39.02700],
+        zoom: 8
+      });
+
+      const secured = new FeatureLayer({
+        url: "http://localhost:5001/ut-dts-agrc-poc-utahid-fb-dev/us-central1/maps/secured/1"
+      });
+
+      map.add(secured);
+    });
+  };
+
+  useEffect(() => {
+    if (user) {
+      interceptor.current = {
+        urls: /secured/,
+        before: (params) => {
+          params.requestOptions.headers = {Authorization: `Bearer ${user.accessToken}`};
+          console.log('headers', params.requestOptions.headers);
+        }
+      }
+    }
+
+    initMap();
+  }, [user]);
 
   useEffect(() => {
     const firebaseApp = initializeApp(secrets.firebaseConfig);
@@ -25,6 +72,7 @@ function App() {
     functionsRef.current = functions;
 
     onAuthStateChanged(auth, user => {
+      console.log('user', user);
       setUser(user);
     });
   }, []);
@@ -44,29 +92,39 @@ function App() {
     console.log('secureResult', secureResult);
   };
 
+  const makeMapServerRequest = async () => {
+    const response = await fetch('http://localhost:5001/ut-dts-agrc-poc-utahid-fb-dev/us-central1/mapserver/service1?f=json');
+
+    console.log('response', await response.text());
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>UtahID auth via Firebase POC</h1>
-        <p>
-          {user ? user.email : 'not logged in'}
-        </p>
-        <p>
-          <button type="button" onClick={logIn}>
-            login
-          </button>
-        </p>
-        <p>
-          <button type="button" onClick={() => signOut(authRef.current)}>
-            logout
-          </button>
-        </p>
-        <p>
-          <button type='button' onClick={makeSecureRequest}>
-            make secure request
-          </button>
-        </p>
-      </header>
+    <div>
+      <h1>UtahID auth via Firebase POC</h1>
+      <p>
+        {user ? user.email : 'not logged in'}
+      </p>
+      <p>
+        <button type="button" onClick={logIn}>
+          login
+        </button>
+      </p>
+      <p>
+        <button type="button" onClick={() => signOut(authRef.current)}>
+          logout
+        </button>
+      </p>
+      <p>
+        <button type='button' onClick={makeSecureRequest}>
+          make secure request
+        </button>
+      </p>
+      <p>
+        <button type='button' onClick={makeMapServerRequest}>
+          map server request
+        </button>
+      </p>
+
     </div>
   )
 }
